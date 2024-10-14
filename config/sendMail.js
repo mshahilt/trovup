29f8f932194750271e1f2ff96431b75c813167ad
mshahilt <mshahilt3@gmail.com>
2024-10-14 23:32:11 +0530

    modified

--- config/sendMail.js
+++ config/sendMail.js
@@ -1,27 +1,28 @@
-const nodemailer = require('nodemailer');
-require('dotenv').config();
+const nodemailer = require("nodemailer");
+require("dotenv").config();
 
 // Create a nodemailer transporter
 const transporter = nodemailer.createTransport({
-    service: 'gmail', // or another email service
-    auth: {
-        user: process.env.USER, // Your email address
-        pass: process.env.APP_PASSWORD, // Your email password or app password
-    },
+  service: "gmail",
+  secure: true, // or another email service
+  auth: {
+    user: process.env.USER, // Your email address
+    pass: process.env.APP_PASSWORD, // Your email password or app password
+  },
 });
 
-console.log(process.env.APP_PASSWORD)
+console.log(process.env.APP_PASSWORD);
 
 // Function to send mail
 const sendMail = (mailOptions) => {
-    return new Promise((resolve, reject) => {
-        transporter.sendMail(mailOptions, (err, info) => {
-            if (err) {
-                return reject(err);
-            }
-            resolve(info);
-        });
+  return new Promise((resolve, reject) => {
+    transporter.sendMail(mailOptions, (err, info) => {
+      if (err) {
+        return reject(err);
+      }
+      resolve(info);
     });
+  });
 };
 
 module.exports = sendMail;
