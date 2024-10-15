ebdea14262900ff95efd94fbe8fcfc83f583ace0
mshahilt <mshahilt3@gmail.com>
2024-10-15 18:55:07 +0530

    nodemailer bug test 11

--- config/auth.js
+++ config/auth.js
@@ -7,7 +7,7 @@ const GoogleStrategy = require('passport-google-oauth2').Strategy;
 passport.use(new GoogleStrategy({
     clientID:process.env.GOOGLE_CLIENT_ID,
     clientSecret:process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:5000/auth/google/callback",
+    callbackURL: "https://trovup.online/auth/google/callback",
     passReqToCallback: true
   },
   async function(request, accessToken, refreshToken, profile, done) {
